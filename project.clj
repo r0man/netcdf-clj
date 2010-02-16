fd278b8cb89497ff826e89450b0a5de7089a156a
Roman Scherer <roman.scherer@burningswell.com>
2010-02-16 14:12:10 +0100

    Added netcdf-java dependency.

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
 (defproject netcdf "0.3-SNAPSHOT"
-  :author "Roman Scherer"
-  :description "Rails-like netcdf for Clojure."
+  :description "Clojure NetCDF Library."
   :url "http://github.com/r0man/netcdf-clj"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
-                 [org.apache.maven/maven-ant-tasks "2.0.10"]]
+                 [org.apache.maven/maven-ant-tasks "2.0.10"]
+                 [essi-unidata/netcdf-java "4.0.41"]]
   :dev-dependencies [[autodoc "0.7.0"]
                      [lein-clojars "0.5.0-SNAPSHOT"]
                      [swank-clojure "1.1.0"]]
-  :autodoc {:copyright "Copyright (c) 2010 Roman Scherer"
-            :name "netcdf-clj"})
+  :repositories [["essi-unidata" "http://cropwiki.irri.org/m2repo"]])
