8782b38356269d16eef564f4bc2154e2590452e0
Roman Scherer <roman.scherer@burningswell.com>
2010-02-16 13:57:52 +0100

    Added project file.

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject netcdf "0.3-SNAPSHOT"
+  :author "Roman Scherer"
+  :description "Rails-like netcdf for Clojure."
+  :url "http://github.com/r0man/netcdf-clj"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                 [org.apache.maven/maven-ant-tasks "2.0.10"]]
+  :dev-dependencies [[autodoc "0.7.0"]
+                     [lein-clojars "0.5.0-SNAPSHOT"]
+                     [swank-clojure "1.1.0"]]
+  :autodoc {:copyright "Copyright (c) 2010 Roman Scherer"
+            :name "netcdf-clj"})
