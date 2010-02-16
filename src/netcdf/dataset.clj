d97166b2a7b14502c0b3944c1d46fa6e719441a2
Roman Scherer <roman.scherer@burningswell.com>
2010-02-16 15:49:56 +0100

    Added dataset.

---
+++ src/netcdf/dataset.clj
@@ -0,0 +1,57 @@
+(ns netcdf.dataset
+  (:import ucar.nc2.FileWriter
+           ucar.nc2.NetcdfFile
+           ucar.nc2.dataset.NetcdfDataset
+           [ucar.nc2.dt.grid GridDataset GridAsPointDataset]))
+
+(defn- write-dimensions [dataset writer]
+  (let [dimensions (.getDimensions dataset)]
+    (dorun (map #(. writer writeDimension %) dimensions))
+    (dorun (map #(. writer writeVariable (.findVariable dataset (.getName %))) dimensions))))
+
+(defn- write-global-attributes [dataset writer]
+  (dorun (map #(. writer writeGlobalAttribute %) (.getGlobalAttributes dataset))))
+
+(defn- write-variables [dataset writer variables]
+  (dorun (map #(. writer writeVariable (.findVariable dataset %1)) variables)))
+
+(defmacro with-file-writer [symbol filename & body]
+  `(let [~symbol (ucar.nc2.FileWriter. ~filename false)]
+     (.mkdirs (.getParentFile (java.io.File. ~filename)))
+     ~@body
+     (. ~symbol finish)))
+
+(defn open-grid-dataset
+  "Open the NetCDF grid dataset at the given URI."
+  [uri] (. GridDataset open uri))
+
+(defn open-dataset
+  "Open the NetCDF dataset at the given URI."
+  [uri] (. NetcdfDataset open uri))
+
+(defn close-dataset
+  "Close the given dataset."
+  [dataset] (if dataset (.close dataset)))
+
+(defn copy-dataset
+  "Copy the NetCDF dataset from source to target."
+  [source target variables]
+  (with-open [dataset (open-dataset source)]
+    (with-file-writer writer target
+      (write-global-attributes dataset writer)
+      (write-dimensions dataset writer)
+      (write-variables dataset writer variables))))
+
+(defmulti datatypes
+  "Returns all datatypes in the NetCDF dataset."
+  class)
+
+(defmethod datatypes GridDataset [dataset]
+  (.getGrids dataset))
+
+(defmulti datatype
+  "Returns the datatype in the NetCDF dataset."
+  (fn [dataset variables] (class dataset)))
+
+(defmethod datatype GridDataset [dataset variable]
+  (. dataset findGridDatatype variable))
---
+++ test/netcdf/test/dataset.clj
@@ -0,0 +1,33 @@
+(ns netcdf.test.dataset
+  (:use clojure.test netcdf.dataset))
+
+(def *dataset-uri* "/home/roman/.weather/20100215/nww3.06.nc")
+(def *variable* "htsgwsfc")
+
+(deftest test-open-dataset
+  (let [dataset (open-dataset *dataset-uri*)]
+    (is (= (class dataset) ucar.nc2.NetcdfFile))))
+
+(deftest test-open-grid-dataset
+  (let [dataset (open-grid-dataset *dataset-uri*)]
+    (is (= (class dataset) ucar.nc2.dt.grid.GridDataset))))
+
+(deftest test-close-datatset
+  (let [dataset (open-dataset *dataset-uri*)]
+    (close-dataset dataset))
+  (let [dataset (open-grid-dataset *dataset-uri*)]
+    (close-dataset dataset)))
+
+(deftest test-copy-dataset
+  (let [target "/tmp/.copy-test.netcdf"]
+    (copy-dataset *dataset-uri* target ["htsgwsfc"])
+    (is (= (.exists (java.io.File. target)) true))))
+
+(deftest test-datatypes
+  (let [datatypes (datatypes (open-grid-dataset *dataset-uri*))]
+    (is (every? #(isa? (class %) ucar.nc2.dt.grid.GeoGrid) datatypes))))
+
+(deftest test-datatype
+  (let [datatype (datatype (open-grid-dataset *dataset-uri*) *variable*)]
+    (is (isa? (class datatype) ucar.nc2.dt.grid.GeoGrid))))
+
