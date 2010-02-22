9bc5884e16ebde79982629f3927bda7d11c023a6
Roman Scherer <roman.scherer@burningswell.com>
2010-02-22 17:24:13 +0100

    Implemented render-map with nearest-neighbour interpolation.

--- src/netcdf/render.clj
+++ src/netcdf/render.clj
@@ -3,7 +3,11 @@
            (java.awt Color Dimension)
            (java.awt.event KeyListener)
            (javax.swing JFrame JOptionPane JPanel))
-  (:use netcdf.datatype google.maps.static google.maps.projection netcdf.location))
+  (:use netcdf.datatype
+        google.maps.static
+        google.maps.projection
+        incanter.core
+        netcdf.location))
 
 (defn create-panel [width height]
   (proxy [JPanel KeyListener] [] 
@@ -25,8 +29,7 @@
 
 (defn create-display
   ([] (create-display 360 180))
-  ([width height]
-     (configure-display (JFrame.) (create-panel width height))))
+  ([width height] (configure-display (JFrame.) (create-panel width height))))
 
 (defn value->color [value]
   (cond
@@ -46,15 +49,41 @@
    (> value 0.5) (Color. 32 80 255)
    (> value 0.0) (Color. 5 15 217)
 ;   (> value 0.0) (Color. 152 178 203)   
-   :else Color/black))
+   ;; :else (Color. 153 179 204)
+   :else Color/black
+   ))
 
-;; (defn location->point [location]
-;;   {:x (+ (:longitude location) 180)
-;;    :y (+ (:latitude location) 90)})
+(defn value->color [value]
+  (cond
+   (> value 10.5) (Color. 213 159 0)
+   (> value 9.75) (Color. 213 0 0)
+   (> value 9.0) (Color. 255 0 0)
+   (> value 8.25) (Color. 255 73 0)
+   (> value 7.5) (Color. 255 144 0)
+   (> value 6.75) (Color. 255 196 0)
+   (> value 6.0) (Color. 255 236 0)
+   (> value 5.25) (Color. 255 255 102)
+   (> value 4.5) (Color. 207 255 255)
+   (> value 3.75) (Color. 175 246 255)
+   (> value 3.0) (Color. 157 239 255)
+   (> value 2.25) (Color. 109 193 255)
+   (> value 1.5) (Color. 66 151 255)
+   (> value 0.75) (Color. 32 80 255)
+   ;; (> value 0.5) (Color. 5 15 217)
+   ;; (> value 0.) (Color. 152 178 203)
+   :else (Color. 5 15 217)
+   ;; :else (Color. 153 179 204)
+   ;; :else Color/black
+   ))
+
+(defn water-color? [color]
+  (or (= color (Color. 152 178 203))
+      (= color (Color. 153 179 203))
+      (= color (Color. 153 179 204))))
 
 (defn location->point [location]
-  {:x (* 1(+ (:longitude location) 180))
-   :y (* 1 (+ (* (+ (:latitude location) 90) -1) 180))})
+  {:x (+ (:longitude location) 180)
+   :y (+ (* (+ (:latitude location) 90) -1) 180)})
 
 (defn clear [component]
   (let [bounds (.getBounds component) graphics (.getGraphics component)]
@@ -65,30 +94,41 @@
 (defn make-buffered-image [width height & [type]]  
   (BufferedImage. width height (or type BufferedImage/TYPE_3BYTE_BGR)))
 
-(defn render-data [component data]  
-;  (clear component)
-  (let [graphics (.getGraphics component)]
-    (doseq [{:keys [actual-location value]} data :when (not (.isNaN value))]
-      (let [point (location->point actual-location)]
-        (. graphics setColor (value->color value))
-        (. graphics fillRect (:x point) (:y point) 1 1)))
-    graphics))
+(defmulti render-data 
+  (fn [component data]
+    (cond
+     (isa? (class data) incanter.Matrix) :matrix
+     (seq? data) :seq
+     (vector? data) :vector)))
+
+(defmethod render-data :matrix [component matrix]
+  (let [graphics (.getGraphics component)
+        width (:size (:longitude-axis (meta matrix)))
+        height (:size (:latitude-axis (meta matrix)))]     
+    (doseq [x (range width) y (range height) :let [value (sel matrix x y)] :when (not (.isNaN value))]
+      (. graphics setColor (value->color value))
+      (. graphics fillRect x (- (+ (* -1 y) height) 1) 1 1))))
+
+(defmethod render-data :seq [component sequence]
+  (render-data component (with-meta (apply vector sequence) (meta sequence))))
+
+(defmethod render-data :vector [component vector]
+  (let [graphics (.getGraphics component)
+        width (:size (:longitude-axis (meta vector)))
+        height (:size (:latitude-axis (meta vector)))]     
+    (doseq [index (range (count vector))
+            :let [value (nth vector index)
+                  x (mod index width)
+                  y (/ index width)]
+            :when (not (.isNaN value))]      
+      (. graphics setColor (value->color value))
+      (. graphics fillRect x (+ (* -1 y) height) 1 1))))
 
 (defn render-datatype [component datatype]
-  (render-data component (read-datatype datatype (first (valid-times datatype)))))
+  (render-data component (read-seq datatype (first (valid-times datatype)))))
 
 (defn render-datatypes [component & [datatypes]]
   (doseq [datatype datatypes] (render-datatype component datatype)))
-
-(defn remove-water [source]
-  (let [target (make-buffered-image (.getWidth source) (.getHeight source))
-        graphics (.getGraphics target)]
-    (. graphics drawImage source 0 0 nil)
-    (. graphics setColor Color/black)
-    (doseq [x (range 0 (.getWidth source)) y (range 0 (.getHeight source))]
-      (if (= (Color. (. target getRGB x y)) (Color. 152 178 203))
-        (. graphics fillRect x y 1 1)))
-    target))
 
 (def *datatypes*
      (map #(open-datatype (apply make-datatype %))
@@ -103,22 +143,66 @@
 
 (def *datatype* (nth *datatypes* 0))
 
-(*datatype*)
+(defn render-map [display center valid-time & options]
+  (let [map (apply static-map-image (make-location 0 0) options)
+        graphics (.getGraphics display)
+        options (apply hash-map options)
+        zoom (or (:zoom options) (:zoom *options*))
+        origin (location->coords center zoom)
+        upper-left {:x (- (:x origin) (/ (.getWidth map) 2)) :y (- (:y origin) (/ (.getHeight map) 2))}
+        x-offset (x-coord-delta (:longitude center) (:longitude (coords->location upper-left zoom)) zoom)
+        y-offset (y-coord-delta (:latitude center) (:latitude (coords->location upper-left zoom)) zoom)
+        ]
+    (. graphics drawImage map 0 0 nil)
+    (doseq [y (range 0 (.getHeight map)) x (range 0 (.getWidth map))]
+      (let [location (coords->location {:x (+ (:x origin) x x-offset) :y (+ (:y origin) y y-offset)} zoom)]
+        (if (water-color? (Color. (. map getRGB x y)))
+          (let [data (read-at-location *datatype* valid-time location)]
+            (. graphics setColor (value->color (:value data)))
+            (. graphics fillRect x y 1 1)))))))
 
-(def *display* (create-display))
-(clear *display*)
-(time (render-datatype *display* *datatype*))
+;; (render-datatype *display* *datatype*)
+;; ;; (*datatype*)
 
-(x-coord-delta -180 180 0)
-(longitude-delta -180 180 0)
-(latitude-delta -180 180 0)
+;; (def *display* (create-display 500 300))
+;; ;; (clear *display*)
+;; ;; (time (render-datatype *display* *datatype*))
+;; (clear *display*)
+;; (render-data *display* (read-matrix *datatype* (first (valid-times *datatype*))))
+;; (render-data *display* (read-seq *datatype* (first (valid-times *datatype*))))
 
 
-(def *map* (static-map-image (make-location 0 0) :width 360 :height 180 :zoom 0))
-(. (.getGraphics *display*) drawImage *map* 0 0 nil)
-(. (.getGraphics *display*) drawImage (remove-water *map*) 0 0 nil)
+;; (water-color? (Color. 242 239 233))
+
+
+
+;; (def *display* (create-display 500 400))
+;; (clear *display*)
+;; (render-map *display* {:latitude 0 :longitude 0} (nth (valid-times *datatype*) 5) :zoom 1 :width 500 :height 400)
+
+;; (take 5 (render-map *display* {:latitude 0 :longitude 0} :zoom 2 :width 200 :height 100))
+;; (nth (render-map *display* {:latitude 0 :longitude 0} :zoom 2 :width 360 :height 180) 359)
+;; (nth (render-map *display* {:latitude 0 :longitude 0} :zoom 2 :width 360 :height 180) 1)
+
+;; (x-coord-delta -180 180 0)
+;; (longitude-delta -180 180 0)
+;; (latitude-delta -180 180 0)
+
+
+;; (def *map* (static-map-image (make-location 0 0) :width 360 :height 180 :zoom 0))
+;; (. (.getGraphics *display*) drawImage *map* 0 0 nil)
+;; (. (.getGraphics *display*) drawImage (remove-water *map*) 0 0 nil)
 
 ;; (def *data* (read-datatype *datatype* (first (valid-times *datatype*))))
 
 ;; (time (render-datatypes *display* *datatypes*))
 
+;; (defn remove-water [source]
+;;   (let [target (make-buffered-image (.getWidth source) (.getHeight source))
+;;         graphics (.getGraphics target)]
+;;     (. graphics drawImage source 0 0 nil)
+;;     (. graphics setColor Color/black)
+;;     (doseq [x (range 0 (.getWidth source)) y (range 0 (.getHeight source))]
+;;       (if (= (Color. (. target getRGB x y)) (Color. 153 179 204))
+;;         (. graphics fillRect x y 1 1)))
+;;     target))
