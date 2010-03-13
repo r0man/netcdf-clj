b2607af873fbeff0f47f3e087ee9cd1469809507
Roman Scherer <roman.scherer@burningswell.com>
2010-03-13 18:48:24 +0100

    Added geo gird tests.

--- src/netcdf/geo_grid.clj
+++ src/netcdf/geo_grid.clj
@@ -1,7 +1,11 @@
 (ns netcdf.geo-grid
-  (:import java.util.Date ucar.nc2.dt.grid.GeoGrid incanter.Matrix)
+  (:import incanter.Matrix
+           ucar.nc2.dt.grid.GeoGrid
+           ucar.unidata.geoloc.ProjectionPointImpl
+           java.util.Date)
   (:use incanter.core netcdf.location)
-  (:require [netcdf.dataset :as dataset]))
+  (:require [netcdf.dataset :as dataset]
+            [netcdf.projection :as projection]))
 
 (defn coord-system
   "Returns the coordinate system of the geo grid."
@@ -18,7 +22,8 @@
 (defn lat-axis
   "Returns the latitude axis of the geo grid."
   [#^GeoGrid geo-grid]
-  (let [axis (. (coord-system geo-grid) getYHorizAxis) bounds (bounding-box geo-grid)]
+  (let [axis (. (coord-system geo-grid) getYHorizAxis)
+        bounds (bounding-box geo-grid)]
     {:lat-min (.getLatMin bounds)
      :lat-max (.getLatMax bounds)
      :lat-size (int (.getSize axis))
@@ -27,7 +32,8 @@
 (defn lon-axis
   "Returns the longitude axis of the geo grid."
   [#^GeoGrid geo-grid]
-  (let [axis (. (coord-system geo-grid) getXHorizAxis) bounds (bounding-box geo-grid)]
+  (let [axis (. (coord-system geo-grid) getXHorizAxis)
+        bounds (bounding-box geo-grid)]
     {:lon-min (.getLonMin bounds)
      :lon-max (.getLonMax bounds)
      :lon-size (int (.getSize axis))
@@ -45,14 +51,18 @@
   "Returns the projection of the geo grid."
   [#^GeoGrid geo-grid] (.getProjection (coord-system geo-grid)))
 
+(defn time-axis
+  "Returns the time axis of the geo grid for valid-time."
+  [#^GeoGrid geo-grid] (.getTimeAxis1D (coord-system geo-grid)))
+
 (defn valid-times
   "Returns the valid times in the NetCDF geo grid."
-  [#^GeoGrid geo-grid] (.. (coord-system geo-grid) getTimeAxis1D getTimeDates))
+  [#^GeoGrid geo-grid] (.getTimeDates (time-axis geo-grid)))
 
 (defn time-index
   "Returns the time index into the geo grid for valid-time."
   [#^GeoGrid geo-grid #^Date valid-time]
-  (. (. (coord-system geo-grid) getTimeAxis1D) findTimeIndexFromDate valid-time))
+  (. (time-axis geo-grid) findTimeIndexFromDate valid-time))
 
 (defn- read-xy-data [#^GeoGrid geo-grid #^Date valid-time & [z-index]]  
   (. geo-grid readYXData (time-index geo-grid valid-time) (or z-index 0)))
@@ -62,7 +72,7 @@
   [#^GeoGrid geo-grid & options]
   (let [options (apply hash-map options) valid-time (or (:valid-time options) (first (valid-times geo-grid)))]
     (with-meta (seq (. (read-xy-data geo-grid valid-time (:z-index options)) copyTo1DJavaArray))
-      (merge (axis geo-grid) {:valid-time valid-time}))))
+      (merge (lat-lon-axis geo-grid) {:valid-time valid-time :projection (projection geo-grid)}))))
 
 (defn read-matrix
   "Read the whole geo grid as a matrix."
@@ -71,56 +81,37 @@
     (with-meta (.viewRowFlip (matrix sequence (:lon-size (meta sequence))))
       (meta sequence))))
 
-(defn latitude->row [#^Matrix matrix #^Double latitude]
-  (let [{:keys [lat-max lat-step]} (meta matrix)]
-    (* -1 (- (/ latitude lat-step) lat-max))))
-
-(defn longitude->column [#^Matrix matrix #^Double longitude]
-  (let [{:keys [lon-max lon-step]} (meta matrix)]
-    (/ longitude lon-step)))
-
-(defn location->row-col
-  [#^Matrix matrix location]
-  [(latitude->row matrix (latitude location))
-   (longitude->column matrix (longitude location))])
+(defn location->row-column [#^Matrix matrix location]
+  (let [{:keys [ lat-step lon-step projection]} (meta matrix)
+        [row column] (projection/location->row-column projection location )]
+    [(int (/ (+ (* row -1) (int (/ (nrow *matrix*) 2))) lat-step))
+     (int (/ column lon-step))]))
 
 (defn sel-location
   "Select the data point in the matrix for the location."
-  [#^Matrix matrix location]
-  (let [[row column] (location->row-col matrix location)]
-    (if (and (> column 0) (< column (ncol matrix))
-             (> row 0) (< row (nrow matrix)))
+  [#^Matrix matrix location & options]
+  (let [options (apply hash-map options)
+        [row column] (location->row-column matrix location)]
+    (if (and (>= column 0) (< column (ncol matrix))
+             (>= row 0) (< row (nrow matrix)))
       (sel matrix row column))))
 
 (defn sel-location!
   "Select the data point in the matrix for the location."
-  [#^Matrix matrix location]
-  (let [[row column] (location->row-col matrix location)]
-    (sel matrix row column)))
+  [#^Matrix matrix location]   
+  (apply sel matrix (location->row-column matrix location)))
 
-(time
- (dotimes [i (* 256 256)]
-   (sel-location *matrix* (make-location 0 0))))
+(def *matrix* (read-matrix (open-geo-grid "/home/roman/.weather/20100215/nww3.06.nc" "htsgwsfc")))
 
-;; (sel-location *matrix* (make-location 80 300))
-;; (location->row-col *matrix* (make-location 79 0))
+;; (set! *warn-on-reflection* true)
+;; (sel-location *matrix* (make-location 0 0))
 
-;; (location->row-col *matrix* (make-location 78 1.25))
-;; (location->row-col *matrix* (make-location 78 90))
+;;  (time
+;;   (dotimes [i (* 512 256)]
+;;     (sel-location *matrix* (make-location 0 0))))
 
-;; (longitude->column *matrix* 1.25)
-
-;; (sel-location *matrix* (make-location -79 10))
-
-;; (println (meta *matrix*))
-
-;; (def *matrix* (read-matrix (open-geo-grid "/home/roman/.weather/20100215/nww3.06.nc" "htsgwsfc")))
-
-;; (count *matrix*)
 
 ;; (println
 ;;  (sel *matrix*
 ;;       (range 0 5)
 ;;       (range 0 5)))
-
-
--- test/netcdf/test/geo_grid.clj
+++ test/netcdf/test/geo_grid.clj
@@ -1,8 +1,8 @@
 (ns netcdf.test.geo-grid
+  (:import ucar.unidata.geoloc.Projection)
   (:use clojure.test incanter.core incanter.datasets netcdf.geo-grid netcdf.location)
   (:require [netcdf.dataset :as dataset]))
 
-(def *dataset-uri* "/home/roman/.weather/20100215/akw.06.nc")
 (def *dataset-uri* "/home/roman/.weather/20100215/nww3.06.nc")
 (def *variable* "htsgwsfc")
 
@@ -38,13 +38,17 @@
     (is (= (:lon-size axis) 288))
     (is (= (:lon-step axis) 1.25))))
 
-(deftest test-projection
-  (let [projection (projection (open-example-geo-grid))]
-    (is (isa? (class projection) ucar.unidata.geoloc.ProjectionImpl))))
-
 (deftest test-lat-lon-axis
   (let [geo-grid (open-example-geo-grid)]
     (is (= (lat-lon-axis geo-grid) (merge (lat-axis geo-grid) (lon-axis geo-grid))))))
+
+(deftest test-time-axis
+  (let [geo-grid (open-example-geo-grid)]
+    (is (isa? (class (time-axis geo-grid)) ucar.nc2.dataset.CoordinateAxis1DTime))))
+
+(deftest test-projection
+  (let [projection (projection (open-example-geo-grid))]
+    (is (isa? (class projection) ucar.unidata.geoloc.ProjectionImpl))))
 
 (deftest test-read-seq
   (let [geo-grid (open-example-geo-grid)
@@ -54,6 +58,7 @@
     (is (= (count sequence) 45216))
     (let [m (meta sequence)]
       (is (= (:valid-time m) valid-time))
+      (is (isa? (class (:projection m)) Projection))
       (is (= (:lat-max m) 78))
       (is (= (:lat-min m) -78))
       (is (= (:lat-step m) 1))      
@@ -69,6 +74,7 @@
     (is (every? #(= % 288) (map count matrix)))    
     (let [m (meta matrix)]
       (is (= (:valid-time m) (first (valid-times geo-grid))))
+      (is (isa? (class (:projection m)) Projection))
       (is (= (:lat-max m) 78))
       (is (= (:lat-min m) -78))
       (is (= (:lat-step m) 1))      
@@ -86,148 +92,36 @@
     (is (> (count valid-times) 0))
     (is (every? #(isa? (class %) java.util.Date) valid-times))))
 
-(deftest test-latitude->row
-  (let [matrix (read-matrix (open-example-geo-grid))]
-    (are [latitude row]
-      (is (= (latitude->row matrix latitude) row))
-      78 0
-      0 78
-      -78 156)))
-
-(deftest test-longitude->column
-  (let [matrix (read-matrix (open-example-geo-grid))]
-    (are [longitude column]
-      (is (= (longitude->column matrix longitude) column))
-      0 0
-      (:lon-step (meta matrix)) 1
-      90 72
-      180 144)))
-
-(deftest test-location->row-col
+(deftest test-location->row-column
   (let [matrix (read-matrix (open-example-geo-grid))]
     (are [latitude longitude row column]
-      (is (= (location->row-col matrix (make-location latitude longitude)) [row column]))
+      (is (= (location->row-column matrix (make-location latitude longitude)) [row column]))
       78 0 0 0
-      0 0 78 0
-      -78 0 156 0
-      78 1.25 0 1)))
+      78 0.1 0 0
+      78 1.24 0 0
+      78 1.25 0 1
+      78 180 0 144
+      78 -178.5 0 145
+      78 -1.25 0 287
+      77 0 1 0
+      77.1 0 0 0
+      77.9 0 0 0)))
 
-;; (deftest test-read-matrix
-;;   (let [geo-grid (open-example-geo-grid)
-;;         sequence (read-seq geo-grid)
-;;         matrix (read-matrix geo-grid)]
-;;     (is (matrix? matrix))
-;;     (is (= (count sequence) 4))
-;;     (is (= (count matrix) 2))
-;;     (is (every? #(= % 2) (map count matrix)))    
-;;     (is (= (sel matrix 0 0) (:value (nth sequence 0))))
-;;     (is (= (sel matrix 0 1) (:value (nth sequence 1))))
-;;     (is (= (sel matrix 1 0) (:value (nth sequence 2))))
-;;     (is (= (sel matrix 1 1) (:value (nth sequence 3))))
-;;     (let [m (meta matrix)]
-;;       (is (= (:description m) (description geo-grid)))
-;;       (is (= (:valid-time m) (first (valid-times geo-grid))))
-;;       (is (= (:variable m) (:variable geo-grid)))
-;;       (is (= (:lat-max m) 0))
-;;       (is (= (:lat-min m) (* -1 (:lat-step geo-grid))))
-;;       (is (= (:lat-size m) 2))
-;;       (is (= (:lat-step m) (:lat-step geo-grid)))
-;;       (is (= (:lon-max m) (:lon-step geo-grid)))
-;;       (is (= (:lon-min m) 0))
-;;       (is (= (:lon-size m) 2))
-;;       (is (= (:lon-step m) (:lon-step geo-grid))))))
+(deftest test-sel-location
+  (let [matrix (read-matrix (open-example-geo-grid))]
+    (is (.isNaN (sel-location matrix (make-location 78 0))))
+    (is (.isNaN (sel-location matrix (make-location 78 -1.25))))
+    (is (.isNaN (sel-location matrix (make-location -77 0))))
+    (is (.isNaN (sel-location matrix (make-location -77 -1.25))))
+    (is (= (sel-location matrix (make-location 77 0)) 1.809999942779541))
+    (is (= (sel-location matrix (make-location -70 0)) 1.5800000429153442))))
 
-;; (deftest test-read-matrix
-;;   (let [geo-grid (open-example-geo-grid)
-;;         sequence (read-seq geo-grid)
-;;         matrix (read-matrix geo-grid :width 5 :height 5)]
-;;     (is (matrix? matrix))
-;;     (is (= (count sequence) 4))
-;;     (is (= (count matrix) 5))
-;;     (is (every? #(= % 5) (map count matrix)))    
-;;     (is (= (sel matrix 0 0) (:value (nth sequence 0))))
-;;     (is (= (sel matrix 0 1) (:value (nth sequence 1))))
-;;     (is (= (sel matrix 1 0) (:value (nth sequence 2))))
-;;     (is (= (sel matrix 1 1) (:value (nth sequence 3))))
-;;     (let [m (meta matrix)]
-;;       (is (= (:description m) (description geo-grid)))
-;;       (is (= (:valid-time m) (first (valid-times geo-grid))))
-;;       (is (= (:variable m) (:variable geo-grid)))
-;;       (is (= (:lat-max m) 77))
-;;       (is (= (:lat-min m) 73))
-;;       (is (= (:lat-size m) 5))
-;;       (is (= (:lat-step m) (:lat-step geo-grid)))
-;;       (is (= (:lon-max m) 5))
-;;       (is (= (:lon-min m) 0))
-;;       (is (= (:lon-size m) 5))
-;;       (is (= (:lon-step m) (:lon-step geo-grid))))))
-
-;; (deftest test-read-datapoint-with-geo-grid
-;;   (let [geo-grid (open-example-geo-grid)
-;;         valid-time (first (valid-times geo-grid))]
-;;     (is (read-datapoint geo-grid (make-location 0 0) :valid-time valid-time))
-;;     (is (= -999 (read-datapoint geo-grid (make-location 78 0) :valid-time valid-time :nil -999)))))
-
-;; (deftest test-read-datapoint-with-matrix
-;;   (let [geo-grid (open-example-geo-grid)
-;;         matrix (read-matrix geo-grid)
-;;         valid-time (first (valid-times geo-grid))]
-;;     (is (read-datapoint matrix (make-location 0 0) :valid-time valid-time))
-;;     (is (= -999 (read-datapoint matrix (make-location 78 0) :valid-time valid-time :nil -999)))))
-
-
-
-
-;; (deftest test-geo-grid-subset
-;;   (let [geo-grid (open-example-geo-grid) valid-time (first (valid-times geo-grid))]
-;;     (let [subset (geo-grid-subset geo-grid valid-time (make-location 0 0))]
-;;       (is (= (:dataset-uri subset)) (:dataset-uri geo-grid))
-;;       (is (= (:variable subset) (:variable geo-grid)))
-;;       (is (not (= (:service subset) (:service geo-grid))))
-;;       ;; (is (= (:lat-min subset) 0))
-;;       ;; (is (= (:lat-max subset) 78))
-;;       ;; (is (= (:lat-size subset) 3))
-;;       (is (= (:lon-min subset) 0))
-;;       (is (= (:lon-max subset) 1.25))
-;;       (is (= (:lon-size subset) 2)))
-;;     (let [subset (geo-grid-subset geo-grid valid-time (make-location 78 0) :width 2 :height 3)]
-;;       (is (= (:dataset-uri subset)) (:dataset-uri geo-grid))
-;;       (is (= (:variable subset) (:variable geo-grid)))
-;;       (is (not (= (:service subset) (:service geo-grid))))
-;;       (is (= (:lat-min subset) 76))
-;;       (is (= (:lat-max subset) 78))
-;;       (is (= (:lat-size subset) 3))
-;;       (is (= (:lon-min subset) 0))
-;;       (is (= (:lon-max subset) 1.25))
-;;       (is (= (:lon-size subset) 2)))))
-
-;; (deftest test-location->index
-;;   (let [geo-grid (open-example-geo-grid)]
-;;     (are [latitude longitude x y]
-;;       (is (= (location->index geo-grid (make-location latitude longitude)) {:x x :y y}))
-;;       0 0 0 78
-;;       78 -180 144 156
-;;       78 180 144 156
-;;       78 179 143 156
-;;       -78 180 144 0
-;;       -78 179 143 0
-;;       90 0 0 -1
-;;       -90 0 0 -1)))
-
-;; (deftest test-sample-locations-2x2
-;;   (let [geo-grid (open-example-geo-grid)]
-;;     (is (= (sample-locations geo-grid (make-location 78 0) :width 2 :height 2)
-;;            (map #(apply make-location %)
-;;                 [[78.0 0.0] [78.0 1.25]
-;;                  [77.0 0.0] [77.0 1.25]])))))
-
-;; ;; (deftest test-sample-locations-4x4
-;; ;;   (let [geo-grid (open-example-geo-grid)]
-;; ;;     (is (= (sample-locations geo-gridyyyyyyyyyyyyyyyyyy (make-location 78 0) 4 4)
-;; ;;            (map #(apply make-location %)
-;; ;;                 [[78.0 0.0] [78.0 1.25] [77.0 0.0] [77.0 1.25]
-;; ;;                  [78.0 0.0] [78.0 1.25] [77.0 0.0] [77.0 1.25]
-;; ;;                  [78.0 0.0] [78.0 1.25] [77.0 0.0] [77.0 1.25]
-;; ;;                  [78.0 0.0] [78.0 1.25] [77.0 0.0] [77.0 1.25]
-;; ;;                  ])))))
+(deftest test-sel-location!
+  (let [matrix (read-matrix (open-example-geo-grid))]
+    (is (.isNaN (sel-location! matrix (make-location 78 0))))
+    (is (.isNaN (sel-location! matrix (make-location 78 -1.25))))
+    (is (.isNaN (sel-location! matrix (make-location -77 0))))
+    (is (.isNaN (sel-location! matrix (make-location -77 -1.25))))
+    (is (= (sel-location! matrix (make-location 77 0)) 1.809999942779541))
+    (is (= (sel-location! matrix (make-location -70 0)) 1.5800000429153442))))
 
