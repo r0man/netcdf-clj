125e5bebd006654b3745e1b602204a26027f7739
Roman Scherer <roman@burningswell.com>
2010-08-30 19:19:56 +0200

    Added the projection fn to coord system.

--- src/netcdf/coord_system.clj
+++ src/netcdf/coord_system.clj
@@ -1,6 +1,10 @@
 (ns netcdf.coord-system
   (:import ucar.nc2.dt.GridCoordSystem))
 
+(defn projection
+  "Returns the projection of the coordinate system"
+  [#^GridCoordSystem coord-system] (.getProjection coord-system))
+
 (defn x-y-index
   "Find the x and y indexes of the location."
   [#^GridCoordSystem coord-system location]
--- test/netcdf/test/coord_system.clj
+++ test/netcdf/test/coord_system.clj
@@ -1,8 +1,12 @@
 (ns netcdf.test.coord-system
+  (:import ucar.unidata.geoloc.Projection)
   (:require [netcdf.geo-grid :as grid])
   (:use clojure.test netcdf.coord-system netcdf.test.helper))
 
 (def *coord-system* (grid/coord-system (grid/open-geo-grid *dataset-uri* *variable*)))
+
+(deftest test-projection
+  (is (isa? (class (projection *coord-system*)) Projection)))
 
 (deftest test-x-y-index
   (is (= (x-y-index *coord-system* {:latitude 0 :longitude 0}) [0 78])))
