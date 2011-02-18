cc83e2b3b0f44645f87524f304d3357ca3c5eca1
Roman <roman@burningswell.com>
2011-02-18 12:53:03 +0100

    Added the to-map fn to the bounding box ns.

--- src/netcdf/bounding_box.clj
+++ src/netcdf/bounding_box.clj
@@ -13,3 +13,13 @@
   (if (< (longitude location-1) (longitude location-2))
     (LatLonRect. location-1  location-2)
     (LatLonRect. location-2  location-1)))
+
+(defn to-map [bounding-box]
+  {:north-west {:latitude (.getLatitude (.getUpperLeftPoint bounding-box))
+                :longitude (.getLongitude (.getUpperLeftPoint bounding-box))}
+   :north-east {:latitude (.getLatitude (.getUpperRightPoint bounding-box))
+                :longitude (.getLongitude (.getUpperRightPoint bounding-box)) }
+   :south-east {:latitude (.getLatitude (.getLowerRightPoint bounding-box))
+                :longitude (.getLongitude (.getLowerRightPoint bounding-box))}
+   :south-west {:latitude (.getLatitude (.getLowerLeftPoint bounding-box))
+                :longitude (.getLongitude (.getLowerLeftPoint bounding-box))}})
