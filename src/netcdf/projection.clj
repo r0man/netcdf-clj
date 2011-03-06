d18fc3844b943fd71d877788e3cf946f21304095
Roman <roman@burningswell.com>
2011-03-06 16:03:36 +0100

    Removed comments.

--- src/netcdf/projection.clj
+++ src/netcdf/projection.clj
@@ -19,27 +19,8 @@
   (. projection latLonToProj location (or (ProjectionPointImpl.) point)))
 
 (defn backward-mapping
-  "Transforms mathematically the planar Cartesian coordinates (x, y)
-  of a point on the map plane to a set of geographic
-  coordinates (latitude, longitude) on the curved reference surface."
+  "Transforms the planar Cartesian coordinates (x, y) of a point on
+  the map plane to a set of geographic coordinates (latitude,
+  longitude) on the curved reference surface."
   [^Projection projection ^ProjectionPoint point & [^LatLonPoint location]]
   (. projection projToLatLon point (or location (LatLonPointImpl.))))
-
-;; (def *nww3* (netcdf.geo-grid/open-geo-grid "/tmp/netcdf-test.nc" "htsgwsfc"))
-;;; (def *projection* (.. *nww3* getCoordinateSystem getProjection))
-
-;; (.projToLatLon *projection* 0.0 0.0)
-;; (.projToLatLon *projection* 180.0 0.0)
-;; (.projToLatLon *projection* 157.0 0.0)
-
-;; (seq (float-array 2 [1.1 1.2]))
-
-;; (map seq (.projToLatLon *projection* (into-array [(float-array 2 [0.0 0.0]) (float-array 2 [1.0 1.0])])))
-
-;; *projection*
-
-;; (.latLonToProjRect
-;;  *projection* 
-;;  (LatLonRect.
-;;   (LatLonPointImpl. -78 -180)
-;;   (LatLonPointImpl. 78 180)))
