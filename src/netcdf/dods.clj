8c68f1f36c5c2368b958a62ddf4f9dc0d8a7766d
Roman <roman@burningswell.com>
2011-03-11 12:38:44 +0100

    Cleanup.

--- src/netcdf/dods.clj
+++ src/netcdf/dods.clj
@@ -42,7 +42,7 @@
        :reference-time (parse-reference-time (extract dataset :dods))})))
 
 (defn find-inventory-by-url
-  "Returns the parsed inventory for the url."
+  "Returns the inventory at the url."
   [url]
   (let [inventory-url (inventory-url url)]
     (or (get @*cache* inventory-url)
@@ -51,84 +51,16 @@
          (get @*cache* inventory-url)))))
 
 (defn find-datasets-by-url
-  "Returns all datasets whose dods url matches the given url."
+  "Returns all datasets matching the url."
   [url]
   (sort-by :reference-time
            (filter #(and (:dods %) (.startsWith (:dods %) url))
                    (find-inventory-by-url url))))
 
 (defn find-datasets-by-url-and-reference-time
+  "Returns all datasets matching the url and reference time."
   [url reference-time]
   (sort-by :reference-time
            (filter #(and (:dods %) (.startsWith (:dods %) url)
                          (= (:reference-time %) reference-time))
                    (find-inventory-by-url url))))
-
-;; (count (find-dataset-by-url-and-reference-times-by-url "http://nomads.ncep.noaa.gov:9090/dods/wave/nww3"))
-
-;; (count (inventory "http://nomads.ncep.noaa.gov:9090/dods/wave/akw"))
-;; (inventory "file:/home/roman/workspace/netcdf-clj/test-resources/dods/xml")
-
-;; (def *local-url* (str (System/getProperty "user.home") File/separator ".netcdf"))
-
-;; (defn valid-time->reference-time [valid-time]
-;;   (date-time
-;;    (year valid-time)
-;;    (month valid-time)
-;;    (day valid-time)
-;;    (* (int (/ (hour valid-time) 6)) 6)
-;;    0))
-
-;; (defn dataset-directory [repository valid-time]
-;;   (str (:name repository) (unparse (formatters :basic-date) (valid-time->reference-time valid-time))))
-
-;; (defn dataset-filename [repository valid-time]
-;;   (str (:name repository) "_" (unparse (formatters :hour) (valid-time->reference-time valid-time)) "z"))
-
-;; (defn dataset-url [repository valid-time]
-;;   (str (:url repository) "/" (dataset-directory repository valid-time) "/" (dataset-filename repository valid-time)))
-
-;; (defn parse-dods [xml]
-;;   (xml-> (feed-to-zip xml) :dataset :dods text))
-
-;; (defn parse-reference-times [xml url]
-;;   (map parse-reference-time (sort (filter #(. % startsWith url) (parse-dods xml)))))
-
-;; (defn reference-times [repository]
-;;   (parse-reference-times (inventory-url repository) (:url repository)))
-
-;; (defn latest-reference-time [repository]
-;;   (last (reference-times repository)))
-
-;; (defn local-url [repository valid-time & [variable]]
-;;   (let [reference-time (valid-time->reference-time valid-time)]
-;;     (URI. (str "file:" *local-url* File/separator (:name repository) File/separator
-;;                (if variable (str variable File/separator))
-;;                (unparse (formatters :basic-date) reference-time) File/separator "t"
-;;                (unparse (formatters :hour) reference-time) "z.nc"))))
-
-;; (defn download-variable [repository variable & [reference-time]]
-;;   (let [reference-time (or reference-time (latest-reference-time repository))
-;;         remote (dataset-url repository reference-time)
-;;         local (local-url repository reference-time variable)]
-;;     (if-not (file-exists? local)
-;;       (dataset/copy-dataset remote local [variable]))
-;;     local))
-
-;; (defn find-dataset []
-
-;;   )
-
-;; (inventory-url "http://nomads.ncep.noaa.gov:9090/dods/wave/nww3")
-
-;; ;; (find-datasets "http://nomads.ncep.noaa.gov:9090/dods/wave/nww3")
-
-;; ;; ;; (defserver nomads
-;; ;; ;;   "NOAA Operational Model Archive and Distribution System"
-;; ;; ;;   :hostname "nomads.ncep.noaa.gov"
-;; ;; ;;   :port 9090)
-
-;; ;; (parse-inventory "test-resources/dods/xml")
-
-;; ;; (def uri (java.net.URI. "http://nomads.ncep.noaa.gov:9090/dods/xml"))
-
