bc540d30d5bb1c846535b74b46f795705282ad46
Roman <roman@burningswell.com>
2011-07-25 20:56:18 +0200

    Added download task for leiningen.

---
+++ src/leiningen/netcdf.clj
@@ -0,0 +1,18 @@
+(ns leiningen.netcdf
+  (:use netcdf.model))
+
+(defn download [model & variables]
+  (let [model (or (get @*models* (keyword model))
+                  (throw (Exception. (str "Can't find model: " model))))
+        variable-names (set (or variables (map :name (:variables model))))]
+    (doseq [variable (filter #(contains? variable-names (:name %)) (:variables model))]
+      (download-variable model variable))))
+
+(defn netcdf
+  "NetCDF tasks."
+  [project & [command & args]]
+  (condp = command
+    "download"
+    (let [[model variables] args]
+      (doseq [model (if model [model] (sort (map :name (vals @*models*))))]
+        (apply download model variables)))))
