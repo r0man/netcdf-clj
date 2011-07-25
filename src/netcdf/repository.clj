452f19aa02edbcbf8d31aa8c61f6760e8f960362
Roman <roman@burningswell.com>
2011-07-25 22:00:51 +0200

    Added netcdf-file-seq.

--- src/netcdf/repository.clj
+++ src/netcdf/repository.clj
@@ -7,3 +7,8 @@
 (defn local-repository
   "Make a local repository."
   [directory] (LocalRepository. directory))
+
+(defn netcdf-file-seq
+  "Returns a seq of all NetCDF files in dirctory."
+  [directory]
+  (filter #(.endsWith (str %) ".nc") (file-seq (java.io.File. directory))))
