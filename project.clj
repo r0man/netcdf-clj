7888a855b6d7bb1a9e31032ca87c9276b38566c4
Roman <roman@burningswell.com>
2011-08-12 12:51:04 +0200

    Removed commons-httpclient.

--- project.clj
+++ project.clj
@@ -2,22 +2,18 @@
   :description "Clojure NetCDF Library."
   :url "http://github.com/r0man/netcdf-clj"
   :dependencies [[clj-time "0.3.0"]
-                 [commons-httpclient "3.1"]
                  [google-maps "0.4.1-SNAPSHOT"]
                  [incanter/incanter-core "1.2.3"]
                  [javax.media/jai_core "1.1.3"]
                  [edu.ucar/netcdf "4.3.2"]
-                 ;; [opendap/opendap "2.2"]
                  [org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.slf4j/slf4j-log4j12 "1.6.1"]]
   :dev-dependencies [[clj-time "0.3.0"]
-                     [commons-httpclient "3.1"]
                      [google-maps "0.4.1-SNAPSHOT"]
                      [incanter/incanter-core "1.2.3"]
                      [javax.media/jai_core "1.1.3"]
                      [edu.ucar/netcdf "4.3.2"]
-                     ;; [opendap/opendap "2.2"]
                      [org.clojure/clojure "1.2.1"]
                      [org.clojure/clojure-contrib "1.2.0"]]
   :repositories {"unifi.it" "http://ulisse.pin.unifi.it:8081/nexus/content/groups/open.repos/"})
