7cd148d66678728c524a9d40a5f4ee9d2435178d
Roman <roman@burningswell.com>
2011-08-14 15:12:05 +0200

    Added command line options to download task.

--- src/leiningen/netcdf.clj
+++ src/leiningen/netcdf.clj
@@ -1,19 +1,32 @@
 (ns leiningen.netcdf
-  (:use netcdf.model
+  (:use [clojure.string :only (blank? split)]
+        clojure.contrib.command-line
+        netcdf.model
         netcdf.variable))
 
-(defn download [model & variables]
-  (let [model (or (get @*models* (keyword model))
-                  (throw (Exception. (str "Can't find model: " model))))
-        variable-names (set (or variables (map :name (:variables model))))]
-    (doseq [variable (filter #(contains? variable-names (:name %)) (:variables model))]
-      (download-variable model variable))))
+(defn- parse-models [models]
+  (if-not (blank? models)
+    (map lookup-model (split models #","))
+    (vals @*models*)))
+
+(defn- parse-variables [model variables]
+  (if-not (blank? variables)
+    (let [variables (set (split variables #","))]
+      (filter #(contains? variables (:name %)) (:variables model)))
+    (:variables model)))
+
+(defn download [& args]
+  (with-command-line
+    args
+    "Usage: netcdf download [OPTIONS]"
+    [[models m "The list of models to download (Example: akw,nww3)."]
+     [variables v "The list of variables to download (Example: htsgwsfc,windsfc)."]
+     [reference-time r "The reference time (Example: 2011-03-13T12:00:00Z)."]]
+    (doseq [model (parse-models models) variable (parse-variables model variables)]
+      (download-variable model variable :reference-time reference-time))))
 
 (defn netcdf
   "NetCDF tasks."
   [project & [command & args]]
   (condp = command
-    "download"
-    (let [[model variables] args]
-      (doseq [model (if model [model] (sort (map :name (vals @*models*))))]
-        (apply download model variables)))))
+    "download" (apply download args)))
