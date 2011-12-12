c170c1e65fe2a3b4a4a3e98e11250e1ba77b5f3c
Roman <roman@burningswell.com>
2011-12-12 22:38:40 +0100

    Use when-not.

--- src/netcdf/dataset.clj
+++ src/netcdf/dataset.clj
@@ -65,7 +65,7 @@
        (copy-dataset source target (datatype-names dataset))))
   ([source target variables]
      (try
-       (when (valid-md5-checksum? target)
+       (when-not (valid-md5-checksum? target)
          (with-open [dataset (open-dataset source)]
            (with-file-writer writer target
              (write-global-attributes dataset writer)
