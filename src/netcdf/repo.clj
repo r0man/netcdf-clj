4a9856afc927ee43742689d336ace566b7b599a5
Roman <roman@burningswell.com>
2012-01-04 21:09:39 +0100

    Added tests.

--- src/netcdf/repo.clj
+++ src/netcdf/repo.clj
@@ -1,14 +1,21 @@
-(ns netcdf.repo)
+(ns netcdf.repo
+  (:use netcdf.file))
 
 (defprotocol IRepository
-  (-reference-times [repository model]))
+  (reference-times [repository model]))
 
-(defrecord LocalRepository [root])
+;; LOCAL REPOSITORY
 
-(extend-type LocalRepository
-  IRepository
-  (-reference-times [repository model]))
+(defrecord LocalRepository [url])
+
+(defn local-reference-times [repository model]
+  (netcdf-file-seq (:url repository)))
 
 (defn make-local-repository
   "Make a local repository."
   [url] (LocalRepository. url))
+
+(extend-type LocalRepository
+  IRepository
+  (reference-times [repository model]
+    (local-reference-times repository model)))
--- test/netcdf/test/repo.clj
+++ test/netcdf/test/repo.clj
@@ -1,7 +1,15 @@
 (ns netcdf.test.repo
-  (:use clojure.test
+  (:use [netcdf.model :only (nww3)]
+        clojure.test
         netcdf.repo))
+
+(def test-repository (make-local-repository "/home/roman/.netcdf"))
 
 (deftest test-make-local-repository
   (let [repository (make-local-repository "/home/roman/.netcdf")]
-    (is (instance? netcdf.repo.LocalRepository repository))))
+    (is (instance? netcdf.repo.LocalRepository repository))
+    (is (= "/home/roman/.netcdf" (:url repository)))))
+
+(deftest test-reference-times
+  (let [reference-times (reference-times test-repository nww3)]
+    reference-times))
