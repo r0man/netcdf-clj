e6744990e728f95992e0afd8db1aca7a513c4da2
Roman Scherer <roman.scherer@burningswell.com>
2013-03-31 16:02:28 +0200

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -2,20 +2,20 @@
   :description "Clojure NetCDF Library."
   :min-lein-version "2.0.0"
   :url "http://github.com/r0man/netcdf-clj"
-  :dependencies [[clj-time "0.4.4"]
+  :dependencies [[clj-time "0.5.0"]
                  [commandline-clj "0.1.3"]
                  [digest "1.4.2"]
                  [edu.ucar/netcdf "4.3.15"]
                  [edu.ucar/opendap "4.3.15"]
                  [geocoder-clj "0.1.0"]
                  [incanter/incanter-core "1.4.1"]
-                 [javax.media.jai/com.springsource.javax.media.jai.core "1.1.3"]
-                 [org.clojure/clojure "1.5.0"]
+                 [javax.media/jai-core "1.1.3"]
+                 [org.clojure/clojure "1.5.1"]
                  [org.clojure/core.memoize "0.5.2"]
                  [org.clojure/data.zip "0.1.1"]
                  [org.clojure/tools.logging "0.2.3"]]
   :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.0.7"]
                                   [ch.qos.logback/logback-core "1.0.7"]]
                    :resource-paths ["test-resources"]}}
-  :repositories {"springsource" "http://repository.springsource.com/maven/bundles/external"
+  :repositories {"jboss" "https://repository.jboss.org/nexus/content/repositories/thirdparty-releases"
                  "unidata" "https://artifacts.unidata.ucar.edu/content/repositories/unidata-releases"})
