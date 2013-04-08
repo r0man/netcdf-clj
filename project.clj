94ddfc424a4c2f1c12bba7e2baf550a8fad1e030
Roman Scherer <roman.scherer@burningswell.com>
2013-04-08 21:19:47 +0200

    Updated memoize and logging.

--- project.clj
+++ project.clj
@@ -13,11 +13,11 @@
                  [incanter/incanter-core "1.4.1"]
                  [javax.media/jai-core "1.1.3"]
                  [org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.memoize "0.5.2"]
+                 [org.clojure/core.memoize "0.5.3"]
                  [org.clojure/data.zip "0.1.1"]
-                 [org.clojure/tools.logging "0.2.3"]]
-  :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.0.7"]
-                                  [ch.qos.logback/logback-core "1.0.7"]]
+                 [org.clojure/tools.logging "0.2.6"]]
+  :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.0.11"]
+                                  [ch.qos.logback/logback-core "1.0.11"]]
                    :resource-paths ["test-resources"]}}
   :repositories {"jboss-third-party" "https://repository.jboss.org/nexus/content/repositories/thirdparty-releases"
                  "unidata" "https://artifacts.unidata.ucar.edu/content/repositories/unidata-releases"})
