f09f972ec6b691f3dc6a073cbf03bfcdebb2226d
Roman Scherer <roman@burningswell.com>
2015-01-08 10:25:58 +0100

    Add deploy-repositories

--- project.clj
+++ project.clj
@@ -24,6 +24,7 @@
              :provided {:dependencies [[org.apache.hadoop/hadoop-core "2.0.0-mr1-cdh4.5.0"]
                                        [org.apache.hadoop/hadoop-client "2.0.0-cdh4.5.0"]
                                        [org.apache.hadoop/hadoop-common "2.0.0-cdh4.5.0"]]}}
+  :deploy-repositories [["releases" :clojars]]
   :repositories {"cloudera" "https://repository.cloudera.com/artifactory/cloudera-repos"
                  "jboss-third-party" "https://repository.jboss.org/nexus/content/repositories/thirdparty-releases"
                  "unidata" "https://artifacts.unidata.ucar.edu/content/repositories/unidata-releases"}
