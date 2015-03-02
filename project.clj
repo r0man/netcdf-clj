2106eaf8fad73c5982868f307d3779b5871bbae2
Roman Scherer <roman@burningswell.com>
2015-03-02 21:37:33 +0100

    Version 0.0.8-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject netcdf-clj "0.0.7"
+(defproject netcdf-clj "0.0.8-SNAPSHOT"
   :description "Clojure NetCDF Library."
   :min-lein-version "2.0.0"
   :url "http://github.com/r0man/netcdf-clj"
