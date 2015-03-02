b6a6d67ff4fc982d1c459d561d21e55d6eb46762
Roman Scherer <roman@burningswell.com>
2015-03-02 21:36:26 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,25 +5,25 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[cascalog "2.1.1"]
-                 [clj-time "0.6.0"]
-                 [commandline-clj "0.1.6"]
-                 [digest "1.4.3"]
-                 [edu.ucar/netcdf "4.3.18"]
-                 [edu.ucar/opendap "4.3.16"]
+                 [clj-time "0.9.0"]
+                 [commandline-clj "0.1.7"]
+                 [digest "1.4.4"]
+                 [edu.ucar/netcdf "4.3.23"]
+                 [edu.ucar/opendap "4.5.4"]
                  [edu.ucar/unidataCommon "4.2.20"]
                  [incanter/incanter-core "1.4.1"]
                  [javax.media/jai-core "1.1.3"]
                  [org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.memoize "0.5.6"]
+                 [org.clojure/core.memoize "0.5.7"]
                  [org.clojure/data.zip "0.1.1"]
-                 [org.clojure/tools.logging "0.2.6"]]
-  :plugins [[lein-junit "1.1.3"]]
-  :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.0.13"]
-                                  [ch.qos.logback/logback-core "1.0.13"]]
+                 [org.clojure/tools.logging "0.3.1"]]
+  :plugins [[lein-junit "1.1.8"]]
+  :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.1.2"]
+                                  [ch.qos.logback/logback-core "1.1.2"]]
                    :resource-paths ["test-resources"]}
-             :provided {:dependencies [[org.apache.hadoop/hadoop-core "2.0.0-mr1-cdh4.5.0"]
-                                       [org.apache.hadoop/hadoop-client "2.0.0-cdh4.5.0"]
-                                       [org.apache.hadoop/hadoop-common "2.0.0-cdh4.5.0"]]}}
+             :provided {:dependencies [[org.apache.hadoop/hadoop-core "2.5.0-mr1-cdh5.3.2"]
+                                       [org.apache.hadoop/hadoop-client "2.6.0"]
+                                       [org.apache.hadoop/hadoop-common "2.6.0"]]}}
   :deploy-repositories [["releases" :clojars]]
   :repositories {"cloudera" "https://repository.cloudera.com/artifactory/cloudera-repos"
                  "jboss-third-party" "https://repository.jboss.org/nexus/content/repositories/thirdparty-releases"
