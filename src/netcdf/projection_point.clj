1cb2b70f4778f6f8220d44f74e3079d8bb43eb85
Roman Scherer <roman@burningswell.com>
2015-03-02 21:36:07 +0100

    Get rid of projection/point namespaces

--- src/netcdf/projection_point.clj
+++
@@ -1,9 +0,0 @@
-(ns netcdf.projection-point
-  (:import (ucar.unidata.geoloc ProjectionPoint ProjectionPointImpl)))
-
-(defn projection-point?
-  "Returns true if arg is a projection point, otherwise false."
-  [arg] (instance? ProjectionPoint arg))
-
-(defn make-projection-point [x y]
-  (ProjectionPointImpl. x y))
