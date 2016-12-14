913343e5b9383b648a46f90fbf04fbc84dd8e095
r0man <roman@burningswell.com>
2016-12-14 11:50:51 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,9 +5,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[cascalog "2.1.1"]
-                 [clj-time "0.11.0"]
-                 [commandline-clj "0.2.1"]
-                 [digest "1.4.4"]
+                 [clj-time "0.12.2"]
+                 [digest "1.4.5"]
                  [edu.ucar/netcdf4 "4.6.6"]
                  [edu.ucar/opendap "4.6.6"]
                  [edu.ucar/unidataCommon "4.2.20"]
@@ -15,15 +14,15 @@
                  [javax.media/jai-core "1.1.3"]
                  [org.clojure/clojure "1.8.0"]
                  [org.clojure/core.memoize "0.5.9"]
-                 [org.clojure/data.zip "0.1.1"]
+                 [org.clojure/data.zip "0.1.2"]
                  [org.clojure/tools.logging "0.3.1"]]
   :plugins [[lein-junit "1.1.8"]]
-  :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.1.7"]
-                                  [ch.qos.logback/logback-core "1.1.7"]]
+  :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.1.8"]
+                                  [ch.qos.logback/logback-core "1.1.8"]]
                    :resource-paths ["test-resources"]}
-             :provided {:dependencies [[org.apache.hadoop/hadoop-core "2.6.0-mr1-cdh5.7.0"]
-                                       [org.apache.hadoop/hadoop-client "2.7.2"]
-                                       [org.apache.hadoop/hadoop-common "2.7.2"]]}}
+             :provided {:dependencies [[org.apache.hadoop/hadoop-core "2.6.0-mr1-cdh5.10.1"]
+                                       [org.apache.hadoop/hadoop-client "2.7.3"]
+                                       [org.apache.hadoop/hadoop-common "2.7.3"]]}}
   :deploy-repositories [["releases" :clojars]]
   :repositories {"cloudera" "https://repository.cloudera.com/artifactory/cloudera-repos"
                  "jboss-third-party" "https://repository.jboss.org/nexus/content/repositories/thirdparty-releases"
