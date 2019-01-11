ef4b0ed28d8bf8c4cd11eda582edceb28e59db1d
r0man <roman@burningswell.com>
2019-01-11 13:38:07 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,16 +4,16 @@
   :url "http://github.com/r0man/netcdf-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clj-time "0.14.2"]
-                 [digest "1.4.6"]
+  :dependencies [[clj-time "0.15.1"]
+                 [digest "1.4.8"]
                  [edu.ucar/netcdf4 "4.6.11"]
                  [edu.ucar/opendap "4.6.11"]
                  [edu.ucar/unidataCommon "4.2.20"]
                  [javax.media/jai-core "1.1.3"]
-                 [org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.memoize "0.5.9"]
+                 [org.clojure/clojure "1.10.0"]
+                 [org.clojure/core.memoize "0.7.1"]
                  [org.clojure/data.zip "0.1.2"]
-                 [org.clojure/tools.logging "0.4.0"]]
+                 [org.clojure/tools.logging "0.4.1"]]
   :plugins [[lein-junit "1.1.8"]]
   :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.2.3"]
                                   [ch.qos.logback/logback-core "1.2.3"]
@@ -21,9 +21,9 @@
                    :java-source-paths ["test"]
                    :resource-paths ["test-resources"]}
              :provided {:dependencies [[cascalog "2.1.1"]
-                                       [org.apache.hadoop/hadoop-core "2.6.0-mr1-cdh5.14.0"]
-                                       [org.apache.hadoop/hadoop-client "2.9.0"]
-                                       [org.apache.hadoop/hadoop-common "2.9.0"]]}}
+                                       [org.apache.hadoop/hadoop-core "2.6.0-mr1-cdh5.16.1"]
+                                       [org.apache.hadoop/hadoop-client "3.1.1"]
+                                       [org.apache.hadoop/hadoop-common "3.1.1"]]}}
   :deploy-repositories [["releases" :clojars]]
   :repositories {"cloudera" "https://repository.cloudera.com/artifactory/cloudera-repos"
                  "jboss-third-party" "https://repository.jboss.org/nexus/content/repositories/thirdparty-releases"
