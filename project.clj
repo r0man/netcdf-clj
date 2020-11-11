90f40ab958d3ebc16a4fcfc882c9cd7ef469d054
r0man <roman@burningswell.com>
2020-11-11 18:20:54 +0100

    0.0.19-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject netcdf-clj "0.0.18"
+(defproject netcdf-clj "0.0.19-SNAPSHOT"
   :description "Clojure NetCDF Library."
   :min-lein-version "2.0.0"
   :url "https://github.com/r0man/netcdf-clj"
